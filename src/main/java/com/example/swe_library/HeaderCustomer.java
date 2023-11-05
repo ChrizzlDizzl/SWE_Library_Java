d1b01853ece383a4f5e77deb44501a6247f05ac4
Luca Holder <79474232+grxver7@users.noreply.github.com>
2023-11-05 20:50:13 +0100

    add some button stuff

--- src/main/java/com/example/swe_library/HeaderCustomer.java
+++ src/main/java/com/example/swe_library/HeaderCustomer.java
@@ -8,4 +8,13 @@ public class HeaderCustomer {
     public void logOut(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchScene(actionEvent, "login.fxml");
     }
+    public void homeCustomer(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchScene(actionEvent, "headerCustomer.fxml");
+    }
+    public void showInventoryCustomer(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchScene(actionEvent, "showItemCustomer.fxml");
+    }
+    public void searchItemCustomer(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchScene(actionEvent, "showItemCustomer.fxml");
+    }
 }
--- src/main/java/controller/HeaderEmployee.java
+++ src/main/java/controller/HeaderEmployee.java
@@ -9,4 +9,20 @@ public class HeaderEmployee {
     public void logOut(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchScene(actionEvent, "login.fxml");
     }
+    public void homeEmployee(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchScene(actionEvent, "headerEmployee.fxml");
+    }
+    public void showInventoryEmployee(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchScene(actionEvent, "itemListEmployee.fxml");
+    }
+    public void searchItemEmployee(ActionEvent actionEvent) throws IOException {
+        //erst eingabe noch für Medium ID und dann entsprechendes Buch anzeigen
+        SceneSwitcher.switchScene(actionEvent, "showItemEmployee.fxml");
+    }
+    public void addMedia(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchScene(actionEvent, "addMedia.fxml");
+    }
+    public void rentingOptions(ActionEvent actionEvent) throws IOException {
+        SceneSwitcher.switchScene(actionEvent, "rentingOptions.fxml");
+    }
 }
