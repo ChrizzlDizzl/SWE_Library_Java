8e5950e1e5e2786097491d06e93e6fce6edd22c9
chrz-MG <christian.metzger0711@gmail.com>
2023-11-10 14:11:00 +0100

    Added search function for customer

--- src/main/java/controller/HeaderCustomer.java
+++ src/main/java/controller/HeaderCustomer.java
@@ -1,11 +1,16 @@
 package controller;
 
+import com.example.swe_library.InteractionsMedia;
 import com.example.swe_library.SceneSwitcher;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class HeaderCustomer {
+    @FXML
+    private TextField inputSearch;
     public void logOut(ActionEvent actionEvent) throws IOException {
         SceneSwitcher.switchScene(actionEvent, "login.fxml");
     }
@@ -16,6 +21,8 @@ public class HeaderCustomer {
         SceneSwitcher.switchScene(actionEvent, "showItemCustomer.fxml");
     }
     public void searchItemCustomer(ActionEvent actionEvent) throws IOException {
-        SceneSwitcher.switchScene(actionEvent, "showItemCustomer.fxml");
+        String item = inputSearch.getText();
+        InteractionsMedia.searchItem(item);
+        SceneSwitcher.switchScene(actionEvent, "searchCustomer.fxml");
     }
 }
